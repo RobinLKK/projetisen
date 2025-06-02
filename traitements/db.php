baee199456d92238c41a42e2fa51d9eba4b000d8
RobinLKK <ormg.rob@gmail.com>
2025-06-02 11:41:36 +0200

    Update db.php

--- traitements/db.php
+++ traitements/db.php
@@ -1,8 +1,8 @@
 <?php
-	$servername = "localhost"; 
-	$username = "root"; 
-	$password ="root"; 
-	$database = "Projet25_dbmsd";
+	$servername = "robeuxw977.mysql.db"; 
+	$username = "robeuxw977"; 
+	$password ="Robaxel1209"; 
+	$database = "robeuxw977";
 	
 	$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
